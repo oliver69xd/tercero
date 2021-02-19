f0aa3d857818178a8a056de48338bf907bb2ae73
Oliver Paredes <oliverparedes22@gmail.com>
2021-02-19 08:17:12 -0500

    Listo para insertar datos 19/02/2021

--- clases/acciones.php
+++ clases/acciones.php
@@ -1,8 +1,14 @@
 <?php 
 require_once("Usuarios.php");
 $Usuarios=new Usuarios();
-$datos=mysqli_fetch_all( $Usuarios->lista_usuarios() );
 
+if(isset($_POST['op'])){
+	echo "Oliver Paredes listo para insertar desde php";
+print_r($_POST['datos']);
+
+}else{
+$datos=mysqli_fetch_all( $Usuarios->lista_usuarios() );
 echo json_encode($datos);
+}
 
 ?>
